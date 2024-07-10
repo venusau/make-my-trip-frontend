0bc1ed4406777fb0bb955a624dc8454bf734c396
Vicky Bhattacharya <vickybhattacharya19@gmail.com>
2024-07-10 19:32:24 +0530

    made the admin panel appear in the navbar for admin only

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,53 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { UserContext } from "../context";
-import { useContext } from "react";
 
 function Navbar() {
-  let { state, dispatch } = useContext(UserContext);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const { state, dispatch } = useContext(UserContext);
   const navigate = useNavigate();
-  function renderList() {
-    // state = true
+
+  useEffect(() => {
+    const checkAdminStatus = async () => {
+      try {
+        const response = await fetch("https://make-my-trip-backend.onrender.com/api/admin", {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+          }
+        });
+        const data = await response.json();
+        console.log(data)
+        setIsAdmin(data.isAdmin);
+      } catch (error) {
+        console.error("Error checking admin status:", error);
+      }
+    };
+
     if (state) {
-      return [
+      checkAdminStatus();
+    }
+  }, [state]);
+
+  function renderList() {
+    if (state) {
+      return (
         <>
           <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              to="/userdashboard"
-            >
-              Dashboard
-            </NavLink>
+            <NavLink className="nav-link" to="/userdashboard">Dashboard</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/flights">
-              Flights
-            </NavLink>
+            <NavLink className="nav-link" to="/flights">Flights</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/hotels">
-              Hotels
-            </NavLink>
+            <NavLink className="nav-link" to="/hotels">Hotels</NavLink>
           </li>
+          {isAdmin && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/admin">Admin Panel</NavLink>
+            </li>
+          )}
           <li className="nav-item">
-            <NavLink className="nav-link" to="/admin">
-              Admin Panel
-            </NavLink>
-          </li>
-          <li className="nav-item ">
             <div className="btn border rounded-pill text-end">
               <button
                 className="nav-link"
@@ -45,55 +58,48 @@ function Navbar() {
                 }}
                 style={{height:"4vh", textAlign:"center"}}
               >
-               <span className="pb-1">Logout</span> 
+                <span className="pb-1">Logout</span> 
               </button>
             </div>
           </li>
-        </>,
-      ];
+        </>
+      );
     } else {
-      return [
+      return (
         <>
           <li className="nav-item">
-            <NavLink className="nav-link active" to="/signin">
-              Signin
-            </NavLink>
+            <NavLink className="nav-link active" to="/signin">Signin</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/register">
-              Register
-            </NavLink>
+            <NavLink className="nav-link" to="/register">Register</NavLink>
           </li>
-        </>,
-      ];
+        </>
+      );
     }
   }
+
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div
-          className="container-fluid"
-          style={{ display: "flex", justifyContent: "space-between" }}
-        >
-            <NavLink className="navbar-brand" to={state?"/":"/signin"}>
-              <img
-                src="https://imgak.mmtcdn.com/pwa_v3/pwa_hotel_assets/header/mmtLogoWhite.png"
-                height="30vh"
-                // width="100vw"
-                alt="Make My Trip"
-              />
-            </NavLink>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
+        <div className="container-fluid" style={{ display: "flex", justifyContent: "space-between" }}>
+          <NavLink className="navbar-brand" to={state ? "/" : "/signin"}>
+            <img
+              src="https://imgak.mmtcdn.com/pwa_v3/pwa_hotel_assets/header/mmtLogoWhite.png"
+              height="30vh"
+              alt="Make My Trip"
+            />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">{renderList()}</ul>
           </div>
